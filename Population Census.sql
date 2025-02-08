90f21d6440b74ce01a19017a16e0e1f83c016032
Surya <valmikisuriya@gmail.com>
2025-02-08 14:14:56 +0530

    Add files via upload

---
+++ Population Census.sql
@@ -0,0 +1,54 @@
+# Population Census
+
+/*
+Given two tables, City and Country whose descriptions are given below. Find the sum of population of all the Cities that lies in "Asia" continent.
+
+City
+
++-------------+----------+
+| Field       | Type     |
++-------------+----------+
+| ID          | int(11)  |
+| Name        | char(35) |
+| CountryCode | char(3)  |
+| District    | char(20) |
+| Population  | int(11)  |
++-------------+----------+
+
+Country
+
++----------------+-------------+
+| Field          | Type        |
++----------------+-------------+
+| Code           | char(3)     |
+| Name           | char(52)    |
+| Continent      | char(50)    |
+| Region         | char(26)    |
+| SurfaceArea    | float(10,2) |
+| IndepYear      | smallint(6) |
+| Population     | int(11)     |
+| LifeExpectancy | float(3,1)  |
+| GNP            | float(10,2) |
+| GNPOld         | float(10,2) |
+| LocalName      | char(45)    |
+| GovernmentForm | char(45)    |
+| HeadOfState    | char(60)    |
+| Capital        | int(11)     |
+| Code2          | char(2)     |
++----------------+-------------+
+
+PS: City.CountryCode and Country.Code is same key.
+*/
+
+# SQL Query
+
+SELECT
+     sum(City.Population)
+FROM
+    City
+INNER JOIN
+    Country
+ON
+    City.CountryCode = Country.Code
+WHERE
+    Continent = "Asia";
